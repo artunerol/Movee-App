08ab0d6fa209b2a07a43ab68887098cc5e6cc6ef
Artun Erol <artun.erol@yahoo.com>
2022-01-19 10:21:08 +0300

    TVSeriesMainTabViewController.xib file created and designed

---
+++ Movee-App/Moduls/TVSeriesTab/ViewController/TVSeriesMainTabViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TVSeriesMainTabViewController.swift
+//  Movee-App
+//
+//  Created by Erol, Artun on 19.01.2022.
+//
+
+import UIKit
+
+class TVSeriesMainTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
